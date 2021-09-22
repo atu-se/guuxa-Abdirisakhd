df024b4839997a5c1a5eb587067e154cf6bba465
abdirisakhd <abdirisakjr@gmail.com>
2021-09-22 11:11:41 +0300

    nearly finished

--- server.py
+++ server.py
@@ -8,20 +8,21 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
 
-    conn, addr = s.accept()
-    with conn:
-        print('Connection opened:', addr)
+    
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            print('Connection opened:', addr)
 
-        # you may wish to use full_message
-        # remember that it is a bytes array and if you
-        # compare it to a string, you should convert
-        # the string to a bytearray:  b'mystring'
-        full_message = bytearray()
-        while True:
+            full_message = bytearray()
+
             data = conn.recv(BUFFER_SIZE)
             full_message += data
-            if not data: # if no data, break from loop
-                break
+            
+            if full_message == b'password':                
+                conn.sendall(b'enternow')
+                print("Connection closed.") # print 
+                continue
             conn.sendall(data)
     
-    print("Connection closed.")
+        print("Connection closed.")
